2689549fcc12f4f8cab763b0ff20182ec71951a2
Pablo Alesini <palesini@gmail.com>
2025-03-11 14:45:00 +0900

    Optimize dlib build for Render with full dependencies

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get update
-apt-get install -y libpng-dev libjpeg-dev libopenblas-dev
-pip install -r requirements.txt
+apt-get install -y build-essential cmake libopenblas-dev liblapack-dev libx11-dev libgtk-3-dev libpng-dev libjpeg-dev libatlas-base-dev gfortran
+pip install --verbose -r requirements.txt
